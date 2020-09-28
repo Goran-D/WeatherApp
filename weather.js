3803ab296d87e6e6f544049ce2d178e4f9146501
Goran-D <gogi.delija@gmail.com>
2020-09-28 17:25:12 +0200

    update api key

--- weather.js
+++ weather.js
@@ -1,12 +1,12 @@
 class Weather {
   constructor(city) {
-    this.apiKey = 'e9176cae4c72e668165e0c669fed23c0';
+    this.apiKey = '6be72059c883f9c1ae2e192e2df5c629';
     this.city = city;
   }
 
   // Fetch weather data from Weatherstack API 
   async getWeather() {
-    const response = await fetch(`https://api.weatherstack.com/current?access_key=${this.apiKey}&query=${this.city}`);
+    const response = await fetch(`http://api.weatherstack.com/current?access_key=${this.apiKey}&query=${this.city}`);
 
     const responseData = await response.json();
 
